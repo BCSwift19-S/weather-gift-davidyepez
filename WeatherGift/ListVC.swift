4eb5e4c8bbfe0fc8f0acd3c08d649cdfe325d460
David Yepez <yepezd@bc.edu>
2019-03-11 15:46:49 -0400

    Week 8 Complete

---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,94 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by David Yépez on 3/11/19.
+//  Copyright © 2019 David Yepez. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
+    
+    var locationsArray: [String]()
+    var currentPage = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+
+    }
+    
+
+}
+
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "ToPageVC" {
+        let destination = segue.destination as! PageVC
+        currentPage = (tableView.indexPathForSelectedRow?.row)!
+        destination.currentPage = currentPage
+        destination.locationsArray = locationsArray
+        
+    }
+}
+
+@IBAction func editBarButtonPressed(_ sender: UIBarButtonItem) {
+    if tableView.isEditing == true {
+        tableView.setEditing(false, animated: true)
+        editBarButton.title = "Edit"
+        addBarButton.isEnabled = true
+    } else {
+        tableView.setEditing(true, animated: true)
+        editBarButton.title = "done"
+        addBarButton.isEnabled = false
+    }
+}
+
+
+extension ListVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CocationCell", for: indexPath)
+       cell.textLabel?.text = locationsArray[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            locationsArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: <#T##[IndexPath]#>, with: .fade)
+        }
+    
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = locationsArray[sourceIndexPath.row]
+        locationsArray.remove(at: sourceIndexPath)
+        locationsArray.insert(itemToMove, at: destinationIndexPath.row)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return (indexPath.row != 0 ? true : false)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+       return (indexPath.row != 0 ? true : false)
+    }
+    
+    func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
+        return (proposedDestinationIndexPath.row == 0 ? sourceIndexPath : proposedDestinationIndexPath)
+    }
+    
+}
